5767039721c63b6a1d58e1ec29a332a8168d86f0
Cliffano Subagio <cliffano@gmail.com>
2011-05-08 21:26:44 +1000

    Added unit test for couchtato.js

---
+++ test/unit/couchtato.js
@@ -0,0 +1,33 @@
+var assert = require('assert'),
+    Couchtato = require('../../lib/couchtato').Couchtato,
+    vows = require('vows');
+
+vows.describe('Couchtato').addBatch({
+    'iterate': {
+        'should call stool iterate and execute tasks': function(topic) {
+            var _startKeyDocId, _pageSize, _stool, _doc,
+                options = {
+                    pageSize: 99,
+                    tasks: {
+                        'doSomething': function (stool, doc) {
+                            _stool = stool;
+                            _doc = doc;
+                        }
+                    }
+                },
+                stool = {
+                    iterate: function (startKeyDocId, pageSize, process) {
+                        _startKeyDocId = startKeyDocId;
+                        _pageSize = pageSize;
+                        process([ { doc: { _id: 'a' } } ]);
+                    }
+                },
+                couchtato = new Couchtato(options, stool);
+            couchtato.iterate('abcdef');
+            assert.equal(_startKeyDocId, 'abcdef');
+            assert.equal(_pageSize, 99);
+            assert.isFunction(_stool.iterate);
+            assert.equal(_doc._id, 'a');
+        }
+    }
+}).export(module);
