16ad6fc74a9046d4fa989cab1943b451a33b0e64
Cliffano Subagio <cliffano@gmail.com>
2011-05-19 00:04:45 +1000

    Added count, save, and remove support to couchtato.

--- test/unit/couchtato.js
+++ test/unit/couchtato.js
@@ -43,5 +43,59 @@ vows.describe('Couchtato').addBatch({
             assert.isNotNull(_start);
             assert.isNotNull(_finish);
         }
+    },
+    'save': {
+        'should call stool save with expected params': function (topic) {
+            var _doc, _successCb, _errorCb,
+                stool = {
+                    save: function (doc, successCb, errorCb) {
+                        _doc = doc;
+                        _successCb = successCb;
+                        _errorCb = errorCb;
+                    }
+                },
+                couchtato = new Couchtato({}, stool, {});
+            couchtato.save({ message: 'blah' });
+            assert.equal(_doc.message, 'blah');
+            assert.isFunction(_successCb);
+            assert.isFunction(_errorCb);
+        }
+    },
+    'remove': {
+        'should call stool remove with expected params': function (topic) {
+            var _doc, _successCb, _errorCb,
+                stool = {
+                    remove: function (doc, successCb, errorCb) {
+                        _doc = doc;
+                        _successCb = successCb;
+                        _errorCb = errorCb;
+                    }
+                },
+                couchtato = new Couchtato({}, stool, {});
+            couchtato.remove({ message: 'blah' });
+            assert.equal(_doc.message, 'blah');
+            assert.isFunction(_successCb);
+            assert.isFunction(_errorCb);
+        }
+    },
+    'count': {
+        'should call report count with specified key': function (topic) {
+            var _keys = [],
+                report = {
+                    count: function (key) {
+                        _keys.push(key);
+                    }
+                },
+                couchtato = new Couchtato({}, {}, report);
+            assert.equal(_keys.length, 0);
+            couchtato.count('abc');
+            couchtato.count('def');
+            assert.equal(_keys.length, 2);
+            assert.equal(_keys[0], 'abc');
+            assert.equal(_keys[1], 'def');
+            couchtato.count('xyz');
+            assert.equal(_keys.length, 3);
+            assert.equal(_keys[2], 'xyz');
+        }
     }
 }).export(module);
