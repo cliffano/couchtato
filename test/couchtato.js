8992410f09145998f7419390279a065e93178f57
Cliffano Subagio <cliffano@gmail.com>
2012-06-04 20:39:00 +1000

    Unot test couchtato#config.

--- test/couchtato.js
+++ test/couchtato.js
@@ -8,8 +8,14 @@ describe('couchtato', function () {
 
   function create(checks, mocks) {
     return sandbox.require('../lib/couchtato', {
-      requires: mocks ? mocks.requires : {},
-      globals: {}
+      requires: mocks.requires,
+      globals: {
+        console: bag.mock.console(checks),
+        process: bag.mock.process(checks, mocks)
+      },
+      locals: {
+        __dirname: '/somedir/couchtato/lib'
+      }
     });
   }
 
@@ -18,9 +24,26 @@ describe('couchtato', function () {
     mocks = {};
   });
 
-  describe('bar', function () {
+  describe('config', function () {
 
-    it('should foo when bar', function (done) {
+    it('should copy sample couchtato.js file to current directory when config is called', function (done) {
+      mocks.requires = {
+        'fs.extra': {
+          copy: function (source, target, cb) {
+            checks.fsx_copy_source = source;
+            checks.fsx_copy_target = target;
+            cb();
+          }
+        }
+      };
+      couchtato = new (create(checks, mocks))();
+      couchtato.config(function () {
+        done();
+      }); 
+      checks.fsx_copy_source.should.equal('/somedir/couchtato/examples/couchtato.js');
+      checks.fsx_copy_target.should.equal('couchtato.js');
+      checks.console_log_messages.length.should.equal(1);
+      checks.console_log_messages[0].should.equal('Creating sample configuration file: couchtato.js');
     });
   });
 });
