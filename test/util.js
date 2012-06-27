09c175f53d7721d8e4f92d892da5f052a40a467c
Cliffano Subagio <cliffano@gmail.com>
2012-06-27 20:42:27 +1000

    Remove getStat, set default queue to empty array.

--- test/util.js
+++ test/util.js
@@ -26,13 +26,13 @@ describe('util', function () {
 
     it('should set stat to increment value when key does not exist', function () {
       util.increment('somekey', 1000);
-      util.getStat().somekey.should.equal(1000);
+      util.stat.somekey.should.equal(1000);
     });
 
     it('should increment stat by specified increment value when key already exists', function () {
       util.increment('somekey', 10);
       util.increment('somekey', 1000);
-      util.getStat().somekey.should.equal(1010);
+      util.stat.somekey.should.equal(1010);
     });
   });
 
@@ -40,27 +40,27 @@ describe('util', function () {
     
     it('should set stat to 1 when key does not exist', function () {
       util.count('somekey');
-      util.getStat().somekey.should.equal(1);
+      util.stat.somekey.should.equal(1);
     });
 
     it('should increment stat by 1 when key already exists', function () {
       util.count('somekey');
       util.count('somekey');
-      util.getStat().somekey.should.equal(2);
+      util.stat.somekey.should.equal(2);
       util.count('somekey');
-      util.getStat().somekey.should.equal(3);
+      util.stat.somekey.should.equal(3);
     });
   });
 
   describe('getStat', function () {
 
     it('should be empty when no initial stat is specified', function () {
-      _.keys(util.getStat()).length.should.equal(0);
+      _.keys(util.stat).length.should.equal(0);
     });
 
     it('should initialise stat when specified', function () {
       util = new (create(checks, mocks))({ foo: 1000 });
-      util.getStat().foo.should.equal(1000);
+      util.stat.foo.should.equal(1000);
     });
   });
 
@@ -68,9 +68,9 @@ describe('util', function () {
 
     it('should add save count when save is called', function () {
       util.save({});
-      util.getStat()._couchtato_save.should.equal(1);
+      util.stat._couchtato_save.should.equal(1);
       util.save({});
-      util.getStat()._couchtato_save.should.equal(2);
+      util.stat._couchtato_save.should.equal(2);
     });
 
     it('should queue doc when save is called', function () {
@@ -88,9 +88,9 @@ describe('util', function () {
 
     it('should add remove count when remove is called', function () {
       util.remove({});
-      util.getStat()._couchtato_remove.should.equal(1);
+      util.stat._couchtato_remove.should.equal(1);
       util.remove({});
-      util.getStat()._couchtato_remove.should.equal(2);
+      util.stat._couchtato_remove.should.equal(2);
     });
 
     it('should queue doc when remove is called', function () {
