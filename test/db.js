f7c97d582a14d94b2c8b97d0a53f106ca80adc8a
Cliffano Subagio <cliffano@gmail.com>
2012-07-02 20:28:40 +1000

    Check endkey doc ID in list options.

--- test/db.js
+++ test/db.js
@@ -18,6 +18,7 @@ describe('db', function () {
               var count = 0;
               return {
                 list: function (opts, cb) {
+                  checks.nano_list_opts = opts;
                   cb(
                     mocks.nano_list_err, 
                     mocks.nano_list_results ? mocks.nano_list_results[count++] : undefined
@@ -47,9 +48,13 @@ describe('db', function () {
         done();
       });
       checks.db_paginate_err.message.should.equal('someerror');
+      should.not.exist(checks.nano_list_opts.startkey_docid);
+      checks.nano_list_opts.limit.should.equal(10001);
+      checks.nano_list_opts.include_docs.should.equal(true);
+      should.not.exist(checks.nano_list_opts.endkey_docid);
     });
 
-    it('should pass all documents to page callback when there is no error', function () {
+    it('should pass all documents to page callback when there is no error', function (done) {
 
       // simulate first page result to include the first doc of second page result
       mocks.nano_list_results = [
@@ -64,7 +69,7 @@ describe('db', function () {
         checks.db_docs = checks.db_docs.concat(docs);
       }
 
-      db.paginate(0, null, undefined, 2, pageCb, function (err) {
+      db.paginate(0, null, 'someendkey', 2, pageCb, function (err) {
         checks.db_paginate_err = err;
         checks.db_docs.length.should.equal(5);
         checks.db_docs[0]._id = 1;
@@ -73,7 +78,12 @@ describe('db', function () {
         checks.db_docs[3]._id = 3;
         checks.db_docs[4]._id = 4;
         should.not.exist(checks.nano_list_err);
+        done();
       });
+      should.not.exist(checks.nano_list_opts.startkey_docid);
+      checks.nano_list_opts.limit.should.equal(3);
+      checks.nano_list_opts.include_docs.should.equal(true);
+      checks.nano_list_opts.endkey_docid.should.equal('someendkey');
     });
   });
 
