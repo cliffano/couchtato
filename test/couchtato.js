ac006e42ba9bcc0c64225a7ad35883681a50aeab
Cliffano Subagio <cliffano@gmail.com>
2013-09-02 20:37:58 +1000

    Prevent log message showing on stdout during unit test.

--- test/couchtato.js
+++ test/couchtato.js
@@ -7,9 +7,11 @@ var buster = require('buster'),
 
 buster.testCase('couchtato - config', {
   setUp: function () {
+    this.mockConsole = this.mock(console);
     this.mockFsx = this.mock(fsx);
   },
   'should delegate to fsx copy when config is called': function (done) {
+    this.mockConsole.expects('log').once().withExactArgs('Creating sample configuration file: couchtato.js');
     this.mockFsx.expects('copy').once().callsArgWith(2, null, 'someresult');
     var couchtato = new Couchtato();
     couchtato.config(function (err, result) {
