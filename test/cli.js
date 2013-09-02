1d303fa70a5757a13092a2f92e47a18e37cfdb84
Cliffano Subagio <cliffano@gmail.com>
2013-09-02 12:01:32 +1000

    Add more cli tests.

--- test/cli.js
+++ test/cli.js
@@ -32,3 +32,59 @@ buster.testCase('cli - config', {
     cli.exec();
   }
 });
+
+buster.testCase('cli - iterate', {
+  setUp: function () {
+    this.mockConsole = this.mock(console);
+  },
+  'should contain iterate command and delegate to couchtato iterate when exec is called': function (done) {
+    this.stub(_cli, 'command', function (base, actions) {
+      actions.commands.iterate.action({
+        configFile: 'examples/couchtato',
+        url: 'http://someurl',
+        batchSize: 10,
+        pageSize: 20,
+        numPages: 30,
+        startKey: 'somestartkey',
+        endKey: 'someendkey',
+        quiet: true
+      });
+    });
+    this.stub(Couchtato.prototype, 'iterate', function (tasks, url, opts, cb) {
+      assert.equals(typeof tasks.all_docs, 'function');
+      assert.equals(url, 'http://someurl');
+      assert.equals(opts.batchSize, 10);
+      assert.equals(opts.pageSize, 20);
+      assert.equals(opts.numPages, 30);
+      assert.equals(opts.startKey, 'somestartkey');
+      assert.equals(opts.endKey, 'someendkey');
+      assert.isTrue(opts.quiet);
+      assert.equals(typeof cb, 'function');
+      done();
+    });
+    cli.exec();
+  },
+  'should use default couchtato.js': function (done) {
+    var mockProcess = this.mock(process);
+    mockProcess.expects('cwd').once().returns('../examples');
+    this.stub(_cli, 'command', function (base, actions) {
+      actions.commands.iterate.action({
+        url: 'http://someurl',
+        quiet: false
+      });
+    });
+    this.stub(Couchtato.prototype, 'iterate', function (tasks, url, opts, cb) {
+      assert.equals(typeof tasks.all_docs, 'function');
+      assert.equals(url, 'http://someurl');
+      assert.equals(opts.batchSize, undefined);
+      assert.equals(opts.pageSize, undefined);
+      assert.equals(opts.numPages, undefined);
+      assert.equals(opts.startKey, undefined);
+      assert.equals(opts.endKey, undefined);
+      assert.isFalse(opts.quiet);
+      assert.equals(typeof cb, 'function');
+      done();
+    });
+    cli.exec();
+  }
+});
