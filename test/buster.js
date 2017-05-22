e1c69cfadbdc29cda449212be2b653c8229d7de3
dsquier <david.squier@toyota.com>
2017-05-22 12:11:19 -0700

    Add util functions, callback response object
    - Adds response object to report callback
    - Adds two util functions: audit, hash with associated tests
    -- util.audit adds an object to an array that is returned upon
    completion
    -- util.hash returns a SHA256 hash using node-object-hash
    - Replaces use of var with const/let where appropriate
    - Removes lodash require() from db.js (not used in file)
    - Upgrades node, async, lodash, nano dependency versions
    - Add local testing instructions, buster-node config file, and command
    for "npm test"
    - Bumped version from 0.2.2 -> 0.3.0

---
+++ test/buster.js
@@ -0,0 +1,12 @@
+var config = module.exports;
+
+config["Couchtato Tests"] = {
+    rootPath: "../",
+    environment: "node", // or "node"
+    sources: [
+        "lib/*.js"
+    ],
+    tests: [
+        "test/*.js"
+    ]
+}
